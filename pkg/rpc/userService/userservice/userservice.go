4ef46acb5abc0c65b64674296f48e44bd4ebb029
oneVegeDog <1475099861@qq.com>
2023-09-23 10:45:21 +0800

    第二次提交

--- pkg/rpc/userService/userservice/userservice.go
+++ pkg/rpc/userService/userservice/userservice.go
@@ -3,16 +3,16 @@
 package userservice
 
 import (
-			"context"
-				client "github.com/cloudwego/kitex/client"
-				kitex "github.com/cloudwego/kitex/pkg/serviceinfo"
-				baseresp "github.com/oneVegeDog/sourceNavigator/pkg/rpc/baseresp"
-				userservice "github.com/oneVegeDog/sourceNavigator/pkg/rpc/userService"
+	"context"
+	client "github.com/cloudwego/kitex/client"
+	kitex "github.com/cloudwego/kitex/pkg/serviceinfo"
+	baseresp "github.com/oneVegeDog/sourceNavigator/pkg/rpc/baseresp"
+	userservice "github.com/oneVegeDog/sourceNavigator/pkg/rpc/userService"
 )
 
 func serviceInfo() *kitex.ServiceInfo {
 	return userServiceServiceInfo
- }
+}
 
 var userServiceServiceInfo = NewServiceInfo()
 
@@ -20,39 +20,34 @@ func NewServiceInfo() *kitex.ServiceInfo {
 	serviceName := "UserService"
 	handlerType := (*userservice.UserService)(nil)
 	methods := map[string]kitex.MethodInfo{
-		"register":
-			kitex.NewMethodInfo(registerHandler, newUserServiceRegisterArgs, newUserServiceRegisterResult, false),
-		"findUserByName":
-			kitex.NewMethodInfo(findUserByNameHandler, newUserServiceFindUserByNameArgs, newUserServiceFindUserByNameResult, false),
-		"checkedUser":
-			kitex.NewMethodInfo(checkedUserHandler, newUserServiceCheckedUserArgs, newUserServiceCheckedUserResult, false),
+		"register":       kitex.NewMethodInfo(registerHandler, newUserServiceRegisterArgs, newUserServiceRegisterResult, false),
+		"findUserByName": kitex.NewMethodInfo(findUserByNameHandler, newUserServiceFindUserByNameArgs, newUserServiceFindUserByNameResult, false),
+		"checkedUser":    kitex.NewMethodInfo(checkedUserHandler, newUserServiceCheckedUserArgs, newUserServiceCheckedUserResult, false),
 	}
 	extra := map[string]interface{}{
-		"PackageName":	 "userservice",
-		"ServiceFilePath": "idl\rpc\user.thrift",
+		"PackageName":     "userservice",
+		"ServiceFilePath": "idl\\rpc\\user.thrift",
 	}
 	svcInfo := &kitex.ServiceInfo{
-		ServiceName: 	 serviceName,
-		HandlerType: 	 handlerType,
-		Methods:     	 methods,
-		PayloadCodec:  	 kitex.Thrift,
+		ServiceName:     serviceName,
+		HandlerType:     handlerType,
+		Methods:         methods,
+		PayloadCodec:    kitex.Thrift,
 		KiteXGenVersion: "v0.7.1",
 		Extra:           extra,
 	}
 	return svcInfo
 }
 
-
-
-func registerHandler(ctx context.Context, handler interface{}, arg, result interface{}) error { 
+func registerHandler(ctx context.Context, handler interface{}, arg, result interface{}) error {
 	realArg := arg.(*userservice.UserServiceRegisterArgs)
 	realResult := result.(*userservice.UserServiceRegisterResult)
 	success, err := handler.(userservice.UserService).Register(ctx, realArg.Req)
 	if err != nil {
-	return err
+		return err
 	}
 	realResult.Success = success
-	return nil 
+	return nil
 }
 func newUserServiceRegisterArgs() interface{} {
 	return userservice.NewUserServiceRegisterArgs()
@@ -62,16 +57,15 @@ func newUserServiceRegisterResult() interface{} {
 	return userservice.NewUserServiceRegisterResult()
 }
 
-
-func findUserByNameHandler(ctx context.Context, handler interface{}, arg, result interface{}) error { 
+func findUserByNameHandler(ctx context.Context, handler interface{}, arg, result interface{}) error {
 	realArg := arg.(*userservice.UserServiceFindUserByNameArgs)
 	realResult := result.(*userservice.UserServiceFindUserByNameResult)
 	success, err := handler.(userservice.UserService).FindUserByName(ctx, realArg.Req)
 	if err != nil {
-	return err
+		return err
 	}
 	realResult.Success = success
-	return nil 
+	return nil
 }
 func newUserServiceFindUserByNameArgs() interface{} {
 	return userservice.NewUserServiceFindUserByNameArgs()
@@ -81,16 +75,15 @@ func newUserServiceFindUserByNameResult() interface{} {
 	return userservice.NewUserServiceFindUserByNameResult()
 }
 
-
-func checkedUserHandler(ctx context.Context, handler interface{}, arg, result interface{}) error { 
+func checkedUserHandler(ctx context.Context, handler interface{}, arg, result interface{}) error {
 	realArg := arg.(*userservice.UserServiceCheckedUserArgs)
 	realResult := result.(*userservice.UserServiceCheckedUserResult)
 	success, err := handler.(userservice.UserService).CheckedUser(ctx, realArg.Req)
 	if err != nil {
-	return err
+		return err
 	}
 	realResult.Success = success
-	return nil 
+	return nil
 }
 func newUserServiceCheckedUserArgs() interface{} {
 	return userservice.NewUserServiceCheckedUserArgs()
@@ -99,7 +92,6 @@ func newUserServiceCheckedUserArgs() interface{} {
 func newUserServiceCheckedUserResult() interface{} {
 	return userservice.NewUserServiceCheckedUserResult()
 }
-
 
 type kClient struct {
 	c client.Client
@@ -111,8 +103,7 @@ func newServiceClient(c client.Client) *kClient {
 	}
 }
 
-
-func (p *kClient) Register(ctx context.Context , req *userservice.CreateUserReq) (r *baseresp.Baseresp, err error) {
+func (p *kClient) Register(ctx context.Context, req *userservice.CreateUserReq) (r *baseresp.Baseresp, err error) {
 	var _args userservice.UserServiceRegisterArgs
 	_args.Req = req
 	var _result userservice.UserServiceRegisterResult
@@ -122,7 +113,7 @@ func (p *kClient) Register(ctx context.Context , req *userservice.CreateUserReq)
 	return _result.GetSuccess(), nil
 }
 
-func (p *kClient) FindUserByName(ctx context.Context , req *userservice.FindUserReq) (r *userservice.FindUserResp, err error) {
+func (p *kClient) FindUserByName(ctx context.Context, req *userservice.FindUserReq) (r *userservice.FindUserResp, err error) {
 	var _args userservice.UserServiceFindUserByNameArgs
 	_args.Req = req
 	var _result userservice.UserServiceFindUserByNameResult
@@ -132,7 +123,7 @@ func (p *kClient) FindUserByName(ctx context.Context , req *userservice.FindUser
 	return _result.GetSuccess(), nil
 }
 
-func (p *kClient) CheckedUser(ctx context.Context , req *userservice.CheckUserReq) (r *userservice.CheckUserResp, err error) {
+func (p *kClient) CheckedUser(ctx context.Context, req *userservice.CheckUserReq) (r *userservice.CheckUserResp, err error) {
 	var _args userservice.UserServiceCheckedUserArgs
 	_args.Req = req
 	var _result userservice.UserServiceCheckedUserResult
@@ -141,4 +132,3 @@ func (p *kClient) CheckedUser(ctx context.Context , req *userservice.CheckUserRe
 	}
 	return _result.GetSuccess(), nil
 }
-
