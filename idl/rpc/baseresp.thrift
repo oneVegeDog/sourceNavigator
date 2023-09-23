b9eabcb62f6badb9b9ad1a6b6e9909bb9ed8ea39
oneVegeDog <1475099861@qq.com>
2023-09-23 13:59:14 +0800

    newInit

--- idl/rpc/baseresp.thrift
+++
@@ -1,9 +0,0 @@
-
-
-namespace go baseresp
-include "code.thrift"
-struct baseresp{
-    1:string message
-    2:code.Code statue_code
-    3:i64 service_time
-}
--- idl/rpc/code.thrift
+++
@@ -1,8 +0,0 @@
-
-namespace go code
-
-enum Code{
-    SUCCESS = 200;
-    SERVER_BUSY = 500;
-    NO_POWER = 401;
-}
